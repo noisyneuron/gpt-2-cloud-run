3671d2c23e267e80a75115f4032a8131dd296450
noisyneuron <hello@playdo.io>
2020-05-31 14:28:37 -0400

    test requests

---
+++ app-test.py
@@ -0,0 +1,51 @@
+from starlette.applications import Starlette
+from starlette.responses import UJSONResponse
+import gpt_2_simple as gpt2
+import tensorflow as tf
+import uvicorn
+import os
+import gc
+
+app = Starlette(debug=False)
+
+# sess = gpt2.start_tf_sess(threads=1)
+# gpt2.load_gpt2(sess, model_name="quora-73600", model_dir='/storage/models',)
+
+# Needed to avoid cross-domain issues
+response_header = {
+    'Access-Control-Allow-Origin': '*'
+}
+
+generate_count = 0
+
+
+@app.route('/', methods=['GET', 'POST', 'HEAD'])
+async def homepage(request):
+    global generate_count
+    # global sess
+
+    if request.method == 'GET':
+        params = request.query_params
+    elif request.method == 'POST':
+        params = await request.json()
+    elif request.method == 'HEAD':
+        return UJSONResponse({'text': ''},
+                             headers=response_header)
+
+    text = "This is some text."
+
+    # generate_count += 1
+    # if generate_count == 8:
+    #     # Reload model to prevent Graph/Session from going OOM
+    #     tf.reset_default_graph()
+    #     sess.close()
+    #     sess = gpt2.start_tf_sess(threads=1)
+    #     gpt2.load_gpt2(sess)
+    #     generate_count = 0
+
+    gc.collect()
+    return UJSONResponse({'text': text},
+                         headers=response_header)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
